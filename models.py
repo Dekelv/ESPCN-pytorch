83d8cad3849d86398a934a7d3e9ae1b1f7339fd4
anweshcr7 <anweshcr7@gmail.com>
2020-04-07 16:40:27 +0200

    Added Gelu Activation as an enhancement

--- models.py
+++ models.py
@@ -1,16 +1,23 @@
 import math
 from torch import nn
 
+# Based on the explanation given in the paper,
+# Number of layers (l) = 3
+# kernel, i/p (f_i, n_i) where (5,64) -> (3, 32) -> 3
+# GELU paper https://arxiv.org/pdf/1606.08415v3.pdf
+# GELU incorporates regularisation (dropout) inherently. It demonstrates improvements in Computer Vision tasks.
 
 class ESPCN(nn.Module):
     def __init__(self, scale_factor, num_channels=1):
         super(ESPCN, self).__init__()
         self.first_part = nn.Sequential(
             nn.Conv2d(num_channels, 64, kernel_size=5, padding=5//2),
-            nn.Tanh(),
+            # used GeLU as activation function PSNR -> 32.99
+            nn.GELU(),
             nn.Conv2d(64, 32, kernel_size=3, padding=3//2),
-            nn.Tanh(),
+            nn.GELU(),
         )
+        # pixel shuffle is basically up-sampling the data from LR -> HR
         self.last_part = nn.Sequential(
             nn.Conv2d(32, num_channels * (scale_factor ** 2), kernel_size=3, padding=3 // 2),
             nn.PixelShuffle(scale_factor)
